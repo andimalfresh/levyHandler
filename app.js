75056034cc41ed887cc6fb6b8a8f0727b794c46f
unknown <w.andrew.pedersen@gmail.com>
2019-07-15 09:30:26 -0600

    Error Handler Pushed

---
+++ app.js
@@ -0,0 +1,14 @@
+// The following 2 `app.use`'s MUST follow ALL your routes/middleware
+app.use(notFound)
+app.use(errorHandler)
+
+function notFound(req, res, next) {
+  res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+}
+
+// eslint-disable-next-line
+function errorHandler(err, req, res, next) {
+  console.error('ERROR', err)
+  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({error: err.message, stack, url: req.originalUrl})
+}
